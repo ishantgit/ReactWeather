a8476d0173a76744e59f3de96750d281a6301a5b
ishantrana <ishant@atcouch.com>
2017-05-31 18:43:00 +0530

    example page and adding grid

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = React.createClass({
   render: function(){
     return(
-      <h2>Examples component</h2>
+      <div>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are few examples</p>
+        <ol>
+          <li>
+            <Link to="/?location=delhi">delhi</Link>
+          </li>
+          <li>
+            <Link to="/?location=bangalore">bangalore</Link>
+          </li>
+        </ol>
+      </div>
     );
   }
 });
